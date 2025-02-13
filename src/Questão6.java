e87959ba812d9cf7ac297a20e27cb560f2bb6892
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-02-13 11:43:46 -0300

    resolução da questão 6 feita

--- src/Questão6.java
+++ src/Questão6.java
@@ -5,5 +5,22 @@ public class Questão6 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite uma letra do alfabeto:");
+        String letra = scan.nextLine();
+
+        if(letra.equals("a")) {
+            System.out.println("É uma vogal.");
+        }else if(letra.equals("e")) {
+            System.out.println("É uma vogal.");
+        } else if (letra.equals("i")) {
+            System.out.println("É uma vogal.");
+        }else if(letra.equals("o")) {
+            System.out.println("É uma vogal.");
+        } else if (letra.equals("u")) {
+            System.out.println("É uma vogal.");
+        }else{
+            System.out.println("É uma consoante.");
     }
-}
+    }
+    }
